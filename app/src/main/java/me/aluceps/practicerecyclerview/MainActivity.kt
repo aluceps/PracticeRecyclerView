474dc74ac420eada130ebaf775e729409fedfdeb
Atsushi Takano <aluceps@gmail.com>
2018-12-13 16:02:48 +0900

    add databinding.

--- app/src/main/java/me/aluceps/practicerecyclerview/MainActivity.kt
+++ app/src/main/java/me/aluceps/practicerecyclerview/MainActivity.kt
@@ -1,12 +1,17 @@
 package me.aluceps.practicerecyclerview
 
-import android.support.v7.app.AppCompatActivity
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import me.aluceps.practicerecyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
