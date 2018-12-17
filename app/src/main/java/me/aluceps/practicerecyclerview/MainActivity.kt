6b147f252c25c9eb4d053a8a81d1fb0009e05189
Atsushi Takano <aluceps@gmail.com>
2018-12-17 16:25:42 +0900

    initialize recyclerview.

--- app/src/main/java/me/aluceps/practicerecyclerview/MainActivity.kt
+++ app/src/main/java/me/aluceps/practicerecyclerview/MainActivity.kt
@@ -1,8 +1,13 @@
 package me.aluceps.practicerecyclerview
 
-import androidx.databinding.DataBindingUtil
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Section
+import com.xwray.groupie.ViewHolder
 import me.aluceps.practicerecyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +16,27 @@ class MainActivity : AppCompatActivity() {
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 
+    private var groupAdapter: GroupAdapter<ViewHolder>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        groupAdapter = GroupAdapter()
+        initializeRecyclerView()
+        for (i in 1..5) addItems(i)
+    }
+
+    private fun initializeRecyclerView() {
+        binding.recyclerView.run {
+            adapter = groupAdapter
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            addItemDecoration(DividerItemDecoration(context, LinearLayoutManager.VERTICAL))
+        }
+    }
+
+    private fun addItems(index: Int) {
+        val section = Section()
+        section.setHeader(ItemHeaderViewHolder(index.toLong()).apply { setText("Section No.$index") })
+        for (i in 1..20) section.add(ItemNormalViewHolder(i.toLong()).apply { setText("Section No:$index Row:$i") })
+        groupAdapter?.add(section)
     }
 }
