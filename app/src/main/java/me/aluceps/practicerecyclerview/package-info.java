254a3b51dbe8bbd6377ff6f95aefbe5bcfff9e49
Atsushi Takano <aluceps@gmail.com>
2018-12-23 17:47:52 +0900

    Add package-info.

---
+++ app/src/main/java/me/aluceps/practicerecyclerview/package-info.java
@@ -0,0 +1,7 @@
+@EpoxyDataBindingLayouts({
+        R.layout.item_header,
+        R.layout.item_normal
+})
+package me.aluceps.practicerecyclerview;
+
+import com.airbnb.epoxy.EpoxyDataBindingLayouts;
